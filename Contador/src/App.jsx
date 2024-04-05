0bb214390a3143560a5a7f6b9f91371839ccce83
lmartinez-2022105 <lmartinez-2022105>
2024-04-05 16:33:09 -0600

    Se agregó el primer ejercicio

---
+++ Contador/src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(100)
+
+  function aumentar(){
+    setCount2(count2 + 100);
+  }
+  function restar(){
+    setCount(count - 1);
+    if(count <= -200){
+      window.alert('count can´t be less than -200')
+      setCount(count)
+    }
+  }
+
+  function sumar(){
+    setCount(count + 1);
+    if(count >=  200){
+      window.alert('count can´t be more than 200')
+      setCount(count)
+    }
+  }
+
+  function masOcho(){
+    setCount(count + 8);
+    if(count >=  200){
+      window.alert('count can´t be more than 200')
+      setCount(count)
+    }else{
+      if(count + 8 > 200)
+      setCount(200)
+    }
+  }
+
+  function menosOcho(){
+    setCount(count - 8);
+    if(count <= -200 && count - 8 <= -200){
+      window.alert('count can´t be less than -200')
+      setCount(count)
+    }else{
+      if(count - 8 < -200)
+      setCount(-200)
+    }
+  }
+
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <div className="card-items">
+          <button onClick={menosOcho}>-8</button>
+        <button onClick={restar}>
+         Restar
+        </button>
+        <h3>
+        count is {count}
+        </h3>
+        {/* <p>
+          Contador 2: {count2} 
+        </p> */}
+        <button onClick={sumar}>Sumar</button>
+        <button onClick={masOcho}>+8</button>
+        </div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
